a27c1cc330725493ddd749f9213a9ebf9e6f520e
ketankapale8 <ketan.kapale@gmail.com>
2022-03-30 21:09:20 +0530

    NewStaffMembers
    Signed-off-by: ketankapale8 <ketan.kapale@gmail.com>

--- src/components/NewStaff/index.js
+++ src/components/NewStaff/index.js
@@ -6,14 +6,26 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { Grid } from '@mui/material';
-
-// import AdapterDateFns from '@mui/lab/AdapterDateFns';
-// import LocalizationProvider from '@mui/lab/LocalizationProvider';
-// import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import Autocomplete from '@mui/material/Autocomplete';
 import './styles.css'
 
 export default function NewStaff({ title }) {
   const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const dropdownOptions = [
+    {
+      items: 'item 1'
+    },
+    {
+      items: 'item 2'
+    },
+    {
+      items: 'item 3'
+    }
+  ]
 
   const handleChange = (newValue) => {
     setValue(newValue);
@@ -23,7 +35,7 @@ export default function NewStaff({ title }) {
       sx={{
         width: 1000,
         height: 400,
-        backgroundColor: '#EAEAEA',
+        backgroundColor: '#FFFFF',
       }}
       style={{ marginLeft: 200, marginTop: 40, alignContent: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}
     >
@@ -76,20 +88,47 @@ export default function NewStaff({ title }) {
             />
           </Grid>
 
-          <Grid xs={4} style={{ paddingTop: 20 }}>
-            {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Grid xs={5} style={{ paddingTop: 20 }}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
               <Stack spacing={3}>
                 <DesktopDatePicker
-                  label="Date desktop"
+                  label="Start Date"
                   inputFormat="MM/dd/yyyy"
                   value={value}
                   onChange={handleChange}
                   renderInput={(params) => <TextField {...params} />}
                 />
               </Stack>
-            </LocalizationProvider> */}
+            </LocalizationProvider>
           </Grid>
         </Grid>
+
+        <Grid container spacing={2} style={{ paddingTop: 35 }} >
+          <Grid xs={4}>
+            <Stack spacing={3} sx={{ width: 195 }}>
+              <Autocomplete
+                id="work-role"
+                Work Role
+                options={dropdownOptions.map((option) => option.items)}
+                renderInput={(params) => <TextField {...params} label="Work Role" />}
+              />
+            </Stack>
+          </Grid>
+
+          <Grid xs={4}>
+            <Stack spacing={2} sx={{ width: 405 }}>
+              <Autocomplete
+                id="work-role"
+                Work Role
+                options={dropdownOptions.map((option) => option.items)}
+                renderInput={(params) => <TextField {...params} label="Community Center" />}
+              />
+            </Stack>
+          </Grid>
+
+        </Grid>
+
+
       </div>
 
     </Box>
